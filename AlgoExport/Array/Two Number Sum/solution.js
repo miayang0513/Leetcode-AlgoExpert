741410bcaf2163b3ff78a38d6682ce711bd5f2cc
miayang0513 <a16168972@gmail.com>
2022-05-28 22:02:25 +0800

    AlgoExpert[Array][Easy]: Two Number Sum

---
+++ AlgoExport/Array/Two Number Sum/solution.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @param {number[]} array 
+ * @param {number} targetSum 
+ * @returns
+ */
+function twoNumberSum(array, targetSum) {
+  if (array.length === 1) {
+    return []
+  }
+
+  const map = {}
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] in map) {
+      return [array[i], array[map[array[i]]]]
+    }
+    map[targetSum - array[i]] = i
+  }
+
+  return []
+}
