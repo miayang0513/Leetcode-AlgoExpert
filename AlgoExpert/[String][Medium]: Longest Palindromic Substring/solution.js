51d5fb4283b20000287156b80d6c46f9b6515cab
miayang0513 <a16168972@gmail.com>
2022-11-10 18:39:31 +0000

    AlgoExpert[String][Medium]: Longest Palindromic Substring

--- AlgoExpert/[String][Medium]: Longest Palindromic Substring/solution.js
+++ AlgoExpert/[String][Medium]: Longest Palindromic Substring/solution.js
@@ -4,26 +4,24 @@
  * @returns {string}
  */
 function longestPalindromicSubstring (string) {
-  let longestLeft = 0
-  let longestRight = 0
-  for (let mid = 0.5; mid < string.length - 1; mid += 0.5) {
-    let left = Math.floor(mid) - mid !== 0 ? Math.floor(mid) : mid - 1
-    let right = Math.ceil(mid) - mid !== 0 ? Math.ceil(mid) : mid + 1
-
-    while (left >= 0 && right <= string.length - 1) {
-      if (string[left] !== string[right]) {
-        break
-      }
-
-      if ((longestRight - longestLeft) < (right - left)) {
-        longestLeft = left
-        longestRight = right
-      }
-      left--
-      right++
-    }
+  let longestInterval = [0, 1]
+  for (let i = 0; i < string.length - 1; i++) {
+    const even = getPalindromicInterval(string, i, i + 1)
+    const odd = getPalindromicInterval(string, i, i + 2)
+    const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even
+    longestInterval = longestInterval[1] - longestInterval[0] > longest[1] - longest[0] ? longestInterval : longest
   }
-  return string.slice(longestLeft, longestRight + 1)
+
+  return string.slice(longestInterval[0], longestInterval[1] + 1)
+}
+
+function getPalindromicInterval (string, left, right) {
+  while (left >= 0 && right < string.length) {
+    if (string[left] !== string[right]) break
+    left--
+    right++
+  }
+  return [left + 1, right - 1]
 }
 
 console.log(longestPalindromicSubstring("abaxyzzyxf")) // xyzzyx
