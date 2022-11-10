015fc940c12a3ef6370e7be02766f001920a8f01
miayang0513 <a16168972@gmail.com>
2022-11-10 19:18:52 +0000

    AlgoExpert[String][Medium]: Group Anagrams

--- AlgoExpert/[String][Medium]: Group Anagrams/solution.js
+++ AlgoExpert/[String][Medium]: Group Anagrams/solution.js
@@ -4,21 +4,31 @@
  * @returns {string[][]}
  */
 function groupAnagrams (words) {
-  const anagramMap = {}
-  for (let i = 0; i < words.length; i++) {
-    const keyArray = new Array(26).fill(0)
-    for (const char of words[i]) {
-      keyArray[char.charCodeAt() - 97]++
-    }
-    const key = keyArray.join('')
-    if (key in anagramMap) {
-      anagramMap[key].push(words[i])
+  const anagramsMap = {}
+  for (const word of words) {
+    const key = getAnagramsKey(word)
+    if (key in anagramsMap) {
+      anagramsMap[key].push(word)
     } else {
-      anagramMap[key] = [words[i]]
+      anagramsMap[key] = [word]
     }
   }
 
-  return Object.values(anagramMap)
+  return Object.values(anagramsMap)
+}
+
+function getAnagramsKey (word) {
+  const upper = new Array(26).fill(0)
+  const lower = new Array(26).fill(0)
+  for (const letter of word) {
+    const code = letter.charCodeAt()
+    if (65 <= code && code <= 90) { // A - Z
+      upper[code - 65] += 1
+    } else if (97 <= code && code <= 122) { // a - z
+      lower[code - 97] += 1
+    }
+  }
+  return upper.join('') + lower.join('')
 }
 
 console.log(groupAnagrams(["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"])) // [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
