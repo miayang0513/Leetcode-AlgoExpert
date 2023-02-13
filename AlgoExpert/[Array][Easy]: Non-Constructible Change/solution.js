7f1863f8400cd61c9549499c346b32c779128fa4
miayang0513 <a16168972@gmail.com>
2023-02-13 22:58:45 +0000

    AlgoExpert[Array][Easy]: Non-Constructible Change

--- AlgoExpert/[Array][Easy]: Non-Constructible Change/solution.js
+++ AlgoExpert/[Array][Easy]: Non-Constructible Change/solution.js
@@ -3,17 +3,27 @@
  * @param {number[]} coins 
  * @returns {number}
  */
+
+// 1. array of positive integers (could have multiple coins of same value).
+// 2. return minimum amount of change that you can not create.
+// 3. it can be empty input array.
+
 function nonConstructibleChange(coins) {
-  coins.sort((a, b) => a-b)
+  coins.sort((a, b) => a - b)
 
   let sum = 0
-  for (let i = 0; i < coins.length; i++) {
-    if (sum + 1 < coins[i]) {
+
+  for (const value of coins) {
+    if (sum + 1 < value) {
       break
     }
-    sum += coins[i]
+    sum += value
   }
+
+
   return ++sum
 }
+
+// O(nlogn) time | O(n) space, where n is the number of coins
 
 console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]))
