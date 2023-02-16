57b49bef610107654ff9de2b3db6c57fbb730215
miayang0513 <a16168972@gmail.com>
2023-02-16 06:41:07 +0000

    AlgoExpert[Array][Medium]: Longest Peak

--- AlgoExpert/[Array][Medium]: Longest Peak/README.md
+++ AlgoExpert/[Array][Medium]: Longest Peak/README.md
@@ -25,7 +25,7 @@ array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
  * @returns {number}
  */
 
-function longestPeak (array) {
+function longestPeak(array) {
   let currentLongestPeak = 0
   let i = 1
 
@@ -37,21 +37,23 @@ function longestPeak (array) {
       continue
     }
 
-    let left = i - 1
-    while (left - 1 >= 0 && array[left - 1] < array[left]) {
+    let left = i - 2
+    while (left >= 0 && array[left] < array[left + 1]) {
       left--
     }
 
-    let right = i + 1
-    while (right + 1 < array.length && array[right] > array[right + 1]) {
+    let right = i + 2
+    while (right < array.length && array[right - 1] > array[right]) {
       right++
     }
 
 
-    currentLongestPeak = Math.max(currentLongestPeak, right - left + 1)
+    currentLongestPeak = Math.max(currentLongestPeak, right - left - 1)
     i = right
   }
 
   return currentLongestPeak
 }
+
+// O(n) time | O(1) space, where n is the length of the input array
 ```
--- AlgoExpert/[Array][Medium]: Longest Peak/solution.js
+++ AlgoExpert/[Array][Medium]: Longest Peak/solution.js
@@ -4,7 +4,7 @@
  * @returns {number}
  */
 
-function longestPeak (array) {
+function longestPeak(array) {
   let currentLongestPeak = 0
   let i = 1
 
@@ -16,23 +16,25 @@ function longestPeak (array) {
       continue
     }
 
-    let left = i - 1
-    while (left - 1 >= 0 && array[left - 1] < array[left]) {
+    let left = i - 2
+    while (left >= 0 && array[left] < array[left + 1]) {
       left--
     }
 
-    let right = i + 1
-    while (right + 1 < array.length && array[right] > array[right + 1]) {
+    let right = i + 2
+    while (right < array.length && array[right - 1] > array[right]) {
       right++
     }
 
 
-    currentLongestPeak = Math.max(currentLongestPeak, right - left + 1)
+    currentLongestPeak = Math.max(currentLongestPeak, right - left - 1)
     i = right
   }
 
   return currentLongestPeak
 }
+
+// O(n) time | O(1) space, where n is the length of the input array
 
 console.log(longestPeak([1, 3, 2])) // 3
 console.log(longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3])) // 6
