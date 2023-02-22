4f266841cf3405e761fd5053611124efeca98a66
miayang0513 <a16168972@gmail.com>
2023-02-22 00:16:42 +0000

    AlgoExpert[String][Medium]: Minimum Characters For Words

--- AlgoExpert/[String][Medium]: Minimum Characters For Words/solution.js
+++ AlgoExpert/[String][Medium]: Minimum Characters For Words/solution.js
@@ -3,35 +3,40 @@
  * @param {string[]} words 
  * @returns {string[]}
  */
-function minimumCharactersForWords (words) {
+
+function minimumCharactersForWords(words) {
   const charMap = {}
 
   for (const word of words) {
     const currentCharMap = {}
+
     for (const char of word) {
       if (char in currentCharMap) {
         currentCharMap[char]++
-        if (char in charMap) {
-          charMap[char] = Math.max(currentCharMap[char], charMap[char])
-        }
       } else {
         currentCharMap[char] = 1
-        if (!(char in charMap)) {
-          charMap[char] = 1
-        }
+      }
+    }
+
+    for (const key of Object.keys(currentCharMap)) {
+      if (key in charMap) {
+        charMap[key] = Math.max(charMap[key], currentCharMap[key])
+      } else {
+        charMap[key] = currentCharMap[key]
       }
     }
   }
 
   const answer = []
 
-  for (const char in charMap) {
-    const arr = new Array(charMap[char]).fill(char)
-    answer.push(...arr)
+  for (const key of Object.keys(charMap)) {
+    answer.push(...new Array(charMap[key]).fill(key))
   }
 
   return answer
 }
+
+// O(w*n) time | O(c) space, where w is the length of the words, n is the length of the longest word and c is the number of unique characters.
 
 console.log(minimumCharactersForWords(["this", "that", "did", "deed", "them!", "a"]))
 /**
