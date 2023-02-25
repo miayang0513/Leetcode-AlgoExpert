6c5e6ef5009c7df53ceb6f2cd4d3b5f6720280c0
miayang0513 <a16168972@gmail.com>
2023-02-25 23:51:52 +0000

    AlgoExpert[Linked List][Medium]: Remove Kth Node From End

--- AlgoExpert/[Linked List][Medium]: Remove Kth Node From End/solution.js
+++ AlgoExpert/[Linked List][Medium]: Remove Kth Node From End/solution.js
@@ -11,25 +11,34 @@ class LinkedList {
  * @param {LinkedList} head 
  * @param {number} k
  */
-function removeKthNodeFromEnd (head, k) {
-  let counter = 0
+
+// 1. mutated (no create new)
+// 2. if the head is the node that's supposed to be removed, your function should simply mutate its value and next pointer.
+// 3. linked list have at least k nodes. and k >= 2.
+// 4. doesn't need to return anything.
+
+function removeKthNodeFromEnd(head, k) {
   let first = head
   let second = head
+  let count = 0
 
-  while (counter < k) {
-    second = second.next
-    counter++
-  }
-
-  if (second === null) {
-    head.value = head.next.value
-    head.next = head.next.next
-    return
-  }
-
-  while (second.next !== null) {
-    second = second.next
+  while (count < k) {
+    count++
     first = first.next
   }
-  first.next = first.next.next
+
+  if (first === null) {
+    const next = head.next
+    head.next = next.next
+    head.value = next.value
+    return
+  }
+  
+  while (first.next !== null) {
+    first = first.next
+    second = second.next
+  }
+  second.next = second.next.next
 }
+
+// O(n) time | O(1) space, where n is the number of nodes in the Linked List
