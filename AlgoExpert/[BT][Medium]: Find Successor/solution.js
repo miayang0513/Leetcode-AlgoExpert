e9e05fb16d79ca7f395b524a0cb1ac5c8b40d2e7
miayang0513 <a16168972@gmail.com>
2023-03-05 22:46:32 +0000

    AlgoExpert[BT][Medium]: Find Successor

---
+++ AlgoExpert/[BT][Medium]: Find Successor/solution.js
@@ -0,0 +1,35 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+/**
+ * 
+ * @param {BinaryTree} tree 
+ * @param {BinaryTree} node 
+ * @returns {array}
+ */
+
+function findSuccessor(tree, node) {
+  const getLeftMostChild = (node) => {
+    while (node.left !== null) {
+      node = node.left
+    }
+    return node
+  }
+
+  const getRightMostParent = (node) => {
+    while (node.parent !== null && node.parent.right === node) {
+      node = node.parent
+    }
+    return node.parent
+  }
+  
+  if (node.right !== null) return getLeftMostChild(node.right)
+
+  return getRightMostParent(node)
+}
